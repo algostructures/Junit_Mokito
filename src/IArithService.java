f1603cf7dc9cd15fae0e8abf217d2efc024a3adb
algostructures <saurabh945644@gmail.com>
2017-08-01 00:42:58 +0530

    Initial commit

---
+++ src/IArithService.java
@@ -0,0 +1,9 @@
+/**
+ * Created by ADMIN on 7/28/17.
+ */
+public interface IArithService {
+    int iadd(int x, int y);
+    int isub(int x, int y);
+    int imul(int x, int y);
+    int imod(int x, int y);
+}
